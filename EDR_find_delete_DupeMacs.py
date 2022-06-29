4efa71d3efbdd04418216901aea55bd741e12201
Justin Robert Kennelly <jrkennelly@PCC-Domain.pima.edu>
2022-06-29 15:14:09 -0700

    Added logging to csv
    Logs used for :
     - Pre- deletion records of duplicate
        computers
     - Post - deletion records of duplicate
        computers
     - List of GUIDs deleted & status

--- EDR_find_delete_DupeMacs.py
+++ EDR_find_delete_DupeMacs.py
@@ -37,13 +37,16 @@ def process_response_json(response_json, parsing_container):
         if 'network_addresses' in guid_entry:
             process_guid_json(guid_entry)
 
-def authenticate_and_process_requests():
+def writeCSV_post_deletion_data(client_id, api_key):
+    """ After deleting the duplicate objects this function can request the duplicate information again
+        to validate that the objects were deleted. The information is then logged to a csv.
     """
-    """
-    client_id = input("Enter the Client ID: ")
-    api_key = input("Enter the API Key: ")
+
+    client_id = client_id
+    api_key = api_key
 
     parsed_computers = {}
+    duplicate_computers = set()
 
     # Instantiate requestions session object
     amp_session = requests.session()
@@ -70,7 +73,12 @@ def authenticate_and_process_requests():
         print('Processing index: {}'.format(index))
         process_response_json(response_json, parsed_computers)
 
-    return parsed_computers
+    analyze_parsed_computers(parsed_computers, duplicate_computers)
+    hosts = format_duplicate_container(duplicate_computers)
+
+    # POST - deletion csv data
+    write_to_csv(2, duplicate_computers)
+    
 
 def analyze_parsed_computers(parsed_data, duplicate_container):
     ''' Analyzes the parsed_computers container and looks at how many times each MAC Address
@@ -109,26 +117,6 @@ def print_duplicate_output(formatted_duplicate_container):
         for last_seen, guid  in dupes.items():
             print('  {} - {}'.format(last_seen, guid))
 
-def write_duplicate_json(formatted_duplicate_container):
-    '''Write formatted_duplicate_computers to disk as JSON
-    '''
-    with open('duplicate_hosts.json', 'w') as file:
-        file.write(json.dumps(formatted_duplicate_container))
-
-"""
-def write_parsed_computers(parsed_data):
-    ''' Converts the Set of unique MAC Addresses in the parsed_computers container
-        Without this conversion it can not be written to disk as JSON
-        Writes parsed computers to disk as JSON
-    '''
-    for _, data in parsed_data.items():
-        for key, _ in data['mac_guids'].items():
-            data['mac_guids'][key] = list(data['mac_guids'][key])
-
-    with open('parsed_computers.json', 'w') as file:
-        file.write(json.dumps(parsed_data))
-"""
-
 def extract_target_guids(hosts):
     """ Iterate through the hosts and target the duplicates that have the 
         oldest last_seen times.
@@ -156,15 +144,59 @@ def delete_dupe_guids(session, computers_url, guid_list):
     """ Iterate through  the target list of duplicate computers and
         delte them by connector_guid
     """
-    for guid in guid_list:
-        url = computers_url + '{}'.format(guid)
-        response = session.delete(url)
-        response_json = response.json()
+    #test = True
 
-        if response.status_code == 200 and response_json['data']['deleted']:
-            print('Succesfully deleted {}'.format(guid))
-        else:
-            print('Something went wrong deleting: {}'.format(guid))         
+    # running log during deletion of computer objects
+    with open('target_guids.csv', 'w', newline = '') as csvFile:
+        fieldnames = ['GUID','STATUS']
+        writer = csv.DictWriter(csvFile, fieldnames = fieldnames)
+
+        writer.writeheader()
+        # iterate through list of GUIDS
+        for guid in guid_list:
+            url = computers_url + '{}'.format(guid)
+            #if test:
+            #    print(guid)
+            #else: 
+            response = session.delete(url)
+            response_json = response.json()
+
+            # if response successful: write success status & GUID to csv
+            if response.status_code == 200 and response_json['data']['deleted']:
+                status = 'Successfully deleted: {}'.format(guid)
+                writer.writerow({'GUID': guid, 'STATUS': status})
+            # if response unsuccessful: write status & GUID to csv
+            else:
+                status = 'Something went wrong deleting: {}'.format(guid)
+                writer.writerow({'GUID': guid, 'STATUS': status})
+
+
+def write_to_csv(type, duplicate_container):
+    """Writes data to a csv depending on what type of report is specified
+    """
+    csv_file = ''
+
+    # log to update will either be pre or post guid deletion
+    match type:
+        case 1:
+            # PRE - deletion
+            csv_file = 'duplicate_computers.csv'
+        case 2:
+            # POST - deletion
+            csv_file = 'post_del_report.csv'
+        case _:
+            return 'Invalid type'
+    
+    # open file and set header
+    with open(csv_file, 'w', newline = '') as csvFile:
+        fieldnames = ['HOSTNAME','LAST_SEEN','GUID']
+        writer = csv.DictWriter(csvFile, fieldnames = fieldnames)
+
+        writer.writeheader()
+        # Iterate through list of computers & write each row to csv
+        for host_tuple in sorted(duplicate_container):
+            writer.writerow({'HOSTNAME': host_tuple.hostname, 'LAST_SEEN': host_tuple.last_seen, 'GUID': host_tuple.guid})
+
 
 def get(session, url):
     '''HTTP GET the URL and return the decoded JSON
@@ -176,28 +208,53 @@ def get(session, url):
 def main():
     '''The main logic of the script
     '''
+    client_id = input("Enter the Client ID: ")
+    api_key = input("Enter the API Key: ")
 
-    # Containers for data
     parsed_computers = {}
     duplicate_computers = set()
     target_guids = []
 
+    # Instantiate requestions session object
+    amp_session = requests.session()
+    amp_session.auth = (client_id, api_key)
+
+    # URL to query AMP
+    computers_url = 'https://api.amp.cisco.com/v1/computers/'
+
+    # Query the API
+    response_json = get(amp_session, computers_url)
+
+    # Print the total number of GUIDs found
+    total_guids = response_json['metadata']['results']['total']
+    print('GUIDs found in environment: {}'.format(total_guids))
+
+    # Process the returned JSON
+    process_response_json(response_json, parsed_computers)
+
+    # Check if there are more pages and repeat
+    while 'next' in response_json['metadata']['links']:
+        next_url = response_json['metadata']['links']['next']
+        response_json = get(amp_session, next_url)
+        index = response_json['metadata']['results']['index']
+        print('Processing index: {}'.format(index))
+        process_response_json(response_json, parsed_computers)
+
     # iterate through parsed computers and find duplicate macs
-    parsed_computers = authenticate_and_process_requests()
     analyze_parsed_computers(parsed_computers, duplicate_computers)
 
     # Clean up the duplicate objects
     hosts = format_duplicate_container(duplicate_computers)
-    # display all hosts that have duplicates in the environment
-    # display hosts: last_seen time stamp and Connector_GUID
-    print_duplicate_output(hosts)
+    # write duplicate computer data to csv type 1: PRE - deletion
+    write_to_csv(1, duplicate_computers)
     
-    # iterate through the cleaned up objects and extract the oldest objects connector guids for deletion
+    # iterate through the cleaned up objects and extract the oldest objects connector guids 
     target_guids = extract_target_guids(hosts)
-    for i in target_guids:
-        print(i)
-    # Iterates through the guid list and deletes the computers
+    # Iterates through the guid list and deletes the computers & logs the guid + status
     #delete_dupe_guids(amp_session, computers_url, target_guids)
+
+    # make request for duplicate computer information after deletion
+    writeCSV_post_deletion_data(client_id, api_key) 
 
 
 if __name__ == "__main__":
